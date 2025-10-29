306f723ac224a169f0f5cf14423cc1f276f08755
Arnav Sharma <128791886+arnav1803@users.noreply.github.com>
2025-10-29 08:57:44 +0530

    Time: 375 ms (5.18%), Space: 45.8 MB (25.28%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,23 +1,28 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-        int n=nums.length; int sum=0;
-        int [] pre=new int[n];
-        int [] suf=new int[n];
-        pre[0]=nums[0];
-        for(int i=1;i<n;i++){
-            pre[i]=nums[i]+pre[i-1];
-        }
-        suf[n-1]=nums[n-1];
-        for(int i=n-2;i>=0;i--){
-            suf[i]=nums[i]+suf[i+1];
-        }
-        for(int i=0;i<n;i++){
-            int lsum = (i == 0) ? 0 : pre[i - 1]; 
-            int rsum = (i == n - 1) ? 0 : suf[i + 1];
-            if(lsum==rsum){
+        int n = nums.length;
+
+        // for each index, calculate left sum and right sum separately
+        for (int i = 0; i < n; i++) {
+            int leftSum = 0;
+            int rightSum = 0;
+
+            // compute left sum
+            for (int j = 0; j < i; j++) {
+                leftSum += nums[j];
+            }
+
+            // compute right sum
+            for (int j = i + 1; j < n; j++) {
+                rightSum += nums[j];
+            }
+
+            // check if pivot
+            if (leftSum == rightSum) {
                 return i;
             }
         }
+
         return -1;
     }
 }
