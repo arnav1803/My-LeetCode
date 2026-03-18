808f8ce57c4825c7f51f9a65a466ac7c7313b307
Arnav Sharma <128791886+arnav1803@users.noreply.github.com>
2026-03-19 00:12:04 +0530

    Time: 0 ms (100%), Space: 43.1 MB (97.12%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,17 +1,23 @@
-import java.math.*;
 class Solution {
-    public int[] plusOne(int[] nums) {
-        BigInteger num = BigInteger.ZERO;int i=0;
-        for(i =0;i<nums.length;i++){
-           num = num.multiply(BigInteger.TEN)
-                     .add(BigInteger.valueOf(nums[i]));
+    public int[] plusOne(int[] digits) {
+        int n = digits.length;
+        
+        
+        for (int i = n - 1; i >= 0; i--) {
+           
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            
+          
+            digits[i] = 0;
         }
-        num = num.add(BigInteger.ONE);
-         String s = num.toString();
-        int[] RetArr = new int[s.length()];
-        for (i = 0; i < s.length(); i++) {
-            RetArr[i] = s.charAt(i) - '0';
-        }
-        return RetArr;
+        
+        
+        int[] result = new int[n + 1];
+        result[0] = 1;
+        
+        return result;
     }
 }
