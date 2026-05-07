0d824a081ade5c72bff35f0efe83ca653c4dc156
Arnav Sharma <128791886+arnav1803@users.noreply.github.com>
2026-05-07 09:37:56 +0530

    Time: 4 ms (67.08%), Space: 42 MB (83.35%) - LeetHub

---
+++ 0788-rotated-digits/0788-rotated-digits.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int rotatedDigits(int n) {
+        int count = 0;
+
+        for (int i = 1; i <= n; i++) {
+            int num = i;
+            boolean changed = false;
+            boolean valid = true;
+
+            while (num > 0) {
+                int digit = num % 10;
+
+                if (digit == 3 || digit == 4 || digit == 7) {
+                    valid = false;
+                    break;
+                }
+
+                if (digit == 2 || digit == 5 || digit == 6 || digit == 9) {
+                    changed = true;
+                }
+
+                num /= 10;
+            }
+
+            if (valid && changed) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
