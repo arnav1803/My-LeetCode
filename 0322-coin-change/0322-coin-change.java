ad3fbb436db6e3d669b407acbd9e25d281070b05
Arnav Sharma <128791886+arnav1803@users.noreply.github.com>
2026-08-05 00:35:31 +0530

    Time: 15 ms (82.33%), Space: 46.9 MB (30.05%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -4,11 +4,13 @@
             int dp[][]= new int [n+1][k+1];
             for(int j=1;j<=k;j++)
                 dp[0][j]=Integer.MAX_VALUE-1;
+
             for(int i=1;i<=n;i++){
                 for(int j=1;j<=k;j++){
                     if(nums[i-1]<=j){
                         dp[i][j]=Math.min(1+dp[i][j-nums[i-1]],dp[i-1][j]);
-                    }else{
+                    }
+                    else{
                         dp[i][j]=dp[i-1][j];
                     }
                 }
